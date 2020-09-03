7e896676fa9e69076a0db7fd2afcb5d0498c7f66
yunqian44 <yunqian8@live.com>
2020-09-03 18:32:48 +0800

    add storage service and IService

---
+++ Azure.Storage/Service/AzureStorageConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Service
+{
+    public class AzureStorageConfig
+    {
+        public string ConnectionString { get; set; }
+        public string FileContainerName { get; set; }
+    }
+}

0eecce001d075eee9d8fa6a0e7b2844cb290edc2
yunqian44 <yunqian8@live.com>
2020-09-08 17:37:49 +0800

    Add UserInfo

---
+++ Azure.Storage/Models/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Models
+{
+    public class UserInfo:TableEntity
+    {
+        public UserInfo()
+        {
+
+        }
+
+        public UserInfo(string userName, string IdCardNum)
+        {
+            this.PartitionKey = userName;
+            this.RowKey = IdCardNum;
+
+        }
+
+        public string Email { get; set; }
+
+        public string TelNum { get; set; }
+    }
+}

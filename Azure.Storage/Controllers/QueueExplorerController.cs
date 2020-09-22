dea3484142937bfcc98727287c0bec88590fb544
yunqian44 <yunqian8@live.com>
2020-09-22 18:23:47 +0800

    add QueueController CURL api

---
+++ Azure.Storage/Controllers/QueueExplorerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.Storage.Controllers
+{
+    [Route("Queue")]
+    public class QueueExplorerController : Controller
+    {
+
+        private readonly IQueueService _queueService;
+
+        public QueueExplorerController(IQueueService queueSerivce)
+        {
+            this._queueService = queueSerivce;
+        }
+
+        // GET: QueueExplorerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: QueueExplorerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: QueueExplorerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QueueExplorerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QueueExplorerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: QueueExplorerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QueueExplorerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QueueExplorerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

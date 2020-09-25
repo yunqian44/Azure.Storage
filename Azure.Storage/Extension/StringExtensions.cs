0d3461a06c6dcb5602dfaef1f2de907234bc4be3
yunqian44 <yunqian8@live.com>
2020-09-25 12:02:30 +0800

    添加 Base64 加密，解密方法

---
+++ Azure.Storage/Extension/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Extension
+{
+    public static class StringExtensions
+    {
+        public static string EncryptBase64(this string s)
+        {
+            byte[] b = Encoding.UTF8.GetBytes(s);
+            return Convert.ToBase64String(b);
+        }
+
+        public static string DecodeBase64(this string s)
+        {
+            byte[] b = Convert.FromBase64String(s);
+            return Encoding.UTF8.GetString(b);
+        }
+    }
+}

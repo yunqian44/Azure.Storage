8395ae887ddc7ba1c0e4ed5e4e117c57d52c94d6
yunqian44 <yunqian8@live.com>
2020-09-27 18:12:09 +0800

    add IFileService class and FileService class

---
+++ Azure.Storage/Service/FileService.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Azure.Storage.File;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Service
+{
+    public class FileService : IFileService
+    {
+        private readonly CloudFileClient _cloudFileClient;
+        public FileService(CloudFileClient cloudFileClient)
+        {
+            this._cloudFileClient = cloudFileClient;
+        }
+
+        public Task DeleteFileAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DownFileAsync(string fileName, string downloadPath)
+        {
+            var fileShare = _cloudFileClient.GetShareReference("bloglogfile");
+
+            await fileShare.CreateIfNotExistsAsync();
+
+            if (fileShare.Exists())
+            {
+                var rootDir = fileShare.GetRootDirectoryReference();
+                var portraitDir = rootDir.GetDirectoryReference("portrait");
+                await portraitDir.CreateIfNotExistsAsync();
+
+                if (portraitDir.Exists())
+                {
+                    var file = portraitDir.GetFileReference(fileName);
+
+                    await file.DownloadToFileAsync(downloadPath, FileMode.Create);
+                }
+            }
+        }
+
+        public async Task<MemoryStream> GetFileAsync(string fileName,string downloadPath)
+        {
+            var fileShare = _cloudFileClient.GetShareReference("bloglogfile");
+
+            await fileShare.CreateIfNotExistsAsync();
+            await using var memoryStream = new MemoryStream();
+            if (fileShare.Exists())
+            {
+                var rootDir= fileShare.GetRootDirectoryReference();
+                var portraitDir= rootDir.GetDirectoryReference("portrait");
+                await portraitDir.CreateIfNotExistsAsync();
+
+                if (portraitDir.Exists())
+                {
+                    var file= portraitDir.GetFileReference(fileName);
+                    
+                    await file.DownloadToStreamAsync(memoryStream);
+                    
+                }
+            }
+            return memoryStream;
+        }
+
+        public async Task UpLoadFileAsync(string filePath, string fileName)
+        {
+            var fileShare = _cloudFileClient.GetShareReference("bloglogfile");
+
+            await fileShare.CreateIfNotExistsAsync();
+
+            if (fileShare.Exists())
+            {
+                var rootDir = fileShare.GetRootDirectoryReference();
+                var portraitDir = rootDir.GetDirectoryReference("portrait");
+                await portraitDir.CreateIfNotExistsAsync();
+
+                if (portraitDir.Exists())
+                {
+                    var file = portraitDir.GetFileReference("fileName");
+
+                    await file.UploadFromFileAsync(filePath);
+                }
+            }
+        }
+    }
+}
---
+++ Azure.Storage/Service/IFileService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Service
+{
+    public interface IFileService
+    {
+        Task UpLoadFileAsync(string filePath, string fileName);
+
+        Task DownFileAsync(string fileName, string downloadPath);
+
+        Task<MemoryStream> GetFileAsync(string fileName, string downloadPath);
+
+        Task DeleteFileAsync(string name);
+
+    }
+}
